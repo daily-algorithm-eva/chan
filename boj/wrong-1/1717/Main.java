d779ca1443dbfc1ed2c419b20ccbea4f419d59ce
chan <98894998+longrunpc@users.noreply.github.com>
2025-12-08 11:38:36 +0900

    Solve(disjoint_set): BOJ 1717 "집합의 표현" 문제 풀이 (#15)
    * wrong-1: BOJ 1717 "집합의 표현" 문제 해결 실패
    
    * Solve(disjoint_set): BOJ 1717 "집합의 표현" 문제 풀이

---
+++ boj/wrong-1/1717/Main.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int n, m;
+    static int[] parents;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parents = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parents[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int calc = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (calc == 0) {
+                updateParent(a, b);
+            } else if (calc == 1) {
+                boolean check = checkParent(a, b);
+                if (check) {
+                    sb.append("YES" + "\n");
+                } else {
+                    sb.append("NO" + "\n");
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+
+    public static void updateParent(int a, int b) {
+        while (parents[a] != a) {
+            a = parents[a];
+        }
+        while (parents[b] != b) {
+            b = parents[b];
+        }
+
+        parents[a] = b;
+    }
+
+    public static boolean checkParent(int a, int b) {
+        while (parents[a] != a) {
+            a = parents[a];
+        }
+        while (parents[b] != b) {
+            b = parents[b];
+        }
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+}
