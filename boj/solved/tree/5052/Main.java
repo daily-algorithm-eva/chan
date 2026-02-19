51a557ebdfe8c86ac788c9895b67870c8b12d8d8
longrunpc <pcjmo4050@gmail.com>
2026-02-20 00:16:34 +0900

    Solve(dp): BOJ 1890 "점프" 문제 풀이

---
+++ boj/solved/tree/5052/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int t;
+    static int n;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        t = Integer.parseInt(br.readLine());
+        while(t-- >0) {
+            n = Integer.parseInt(br.readLine());
+            String[] arr = new String[n];
+            for(int i = 0;i<n;i++) {
+                arr[i] = br.readLine();
+            }
+
+            Arrays.sort(arr);
+            boolean check = true;
+            for(int i = 0;i<n-1;i++) {
+                if(arr[i+1].startsWith(arr[i])) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if(check) {
+                sb.append("YES\n");
+            }
+            else {
+                sb.append("NO\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
